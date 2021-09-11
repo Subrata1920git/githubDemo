a4230c499478390820c1d41a8a475a36ff7df53f
subrata kundu <subratakundu1024@gmail.com>
2021-09-11 09:39:52 +0530

    worked on set items in cell

--- githubDemo/ViewController.swift
+++ githubDemo/ViewController.swift
@@ -19,4 +19,23 @@ class ViewController: UIViewController {
 
 
 }
+extension ViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemTableViewCell") as! itemTableViewCell
+        
+        cell.items.text = "item \(indexPath.row+1)"
+        
+       
+        return cell
+    }
+}
+extension ViewController : UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
+}
 
